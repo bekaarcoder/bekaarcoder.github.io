3b01404319cf69b2e123948af1a1709dcc412ad6
Shashank <shashank_smaty@yahoo.co.in>
2017-04-30 07:35:47 +0530

    Added Parallax Effect

--- js/script.js
+++ js/script.js
@@ -41,20 +41,42 @@ $(document).ready(function(){
 		}
 	}
 
-	window.onscroll = loadImages;
+	// window.onscroll = loadImages;
 
-	function parallax() {
-		var wScroll = $(window).scrollTop();
+	// function parallax() {
+	// 	var wScroll = $(window).scrollTop();
 
-		$('.parallax-bg').css('background-position', 'center ' + ((wScroll - 50) * 0.5) + 'px');
-	}
+	// 	$('.parallax-bg').css('background-position', 'center ' + ((wScroll - 50) * 0.5) + 'px');
+	// }
 
-	$(window).scroll(function(){
-		parallax();
-	})
+	// $(window).scroll(function(){
+	// 	parallax();
+	// })
 
 });
 
-var d = new Date();
-var y = d.getFullYear();
-document.getElementById('currYear').innerHTML = y;
+	var d = new Date();
+	var y = d.getFullYear();
+	document.getElementById('currYear').innerHTML = y;
+
+	// parallax-scroll
+
+	window.addEventListener("DOMContentLoaded", scrollLoop, false);
+
+	var parallaximg = document.querySelector("#parallax-img");
+
+	var xScrollPos;
+	var yScrollPos;
+
+	function scrollLoop(e) {
+		xScrollPos = window.scrollX;
+		yScrollPos = window.scrollY;
+
+		setTranslate(0, yScrollPos * -0.5, parallaximg);
+
+		requestAnimationFrame(scrollLoop);
+	}
+
+	function setTranslate(xPos, yPos, el){
+		el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
+	}
